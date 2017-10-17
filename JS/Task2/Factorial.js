d8b5707f2b6587b60284c0e6cc461aa9f68933ca
Igor Kazakov <igor.kazakov157@gmail.com>
2017-10-17 16:03:12 +0200

    "===" used

--- JS/Task2/Factorial.js
+++ JS/Task2/Factorial.js
@@ -3,7 +3,7 @@ var arrSimple = [];
 nextNumber:
     for (var i = 2; true; i++) {
         for (var j = 2; j < i; j++) {
-            if (i % j == 0) continue nextNumber;
+            if (i % j === 0) continue nextNumber;
         }
         if (arrSimple.length < count)
         arrSimple.push(i);
--- JS/Task2/SimpleNumber.js
+++ JS/Task2/SimpleNumber.js
@@ -2,8 +2,8 @@ var count = prompt("Please, enter the count of simple numbers")
 var arrSimple = [];
 for (var i = 2; arrSimple.length < count; i++) {
     for (var j = 2; j < i; j++) {
-        if (i % j == 0) break;
+        if (i % j === 0) break;
     }              
-    if (i == j) arrSimple.push(i);
+    if (i === j) arrSimple.push(i);
 }
 alert(arrSimple);
